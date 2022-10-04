d9644979c71a399b1e43af5aa7bbd194e1e5aa1a
Алиев Ислам <salamvsem4567@gmail.com>
2022-10-05 00:04:02 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="layout">
+    <Header> </Header>
+    <div class="list">
+      <h3 class="list__title">Блюда:</h3>
+      <div class="list__body">
+        <ul>
+          <li>Горячее</li>
+          <li>Супы</li>
+          <li>Вино</li>
+          <li>Мясо</li>
+          <li>Горячее</li>
+          <li>Супы</li>
+          <li>Вино</li>
+          <li>Мясо</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./components/UI/Header.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Header },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap");
+
+* {
+  margin: 0;
+  border: 0;
+  padding: 0;
+  outline: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  font-weight: 500;
+  font-size: 24px;
+}
+
+.layout {
+  max-width: 500px;
+  margin: 0 auto;
+  background: #2c3e50;
+  min-height: 100vh;
+  color: #fff;
+}
+
+.list {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 25px;
+}
+
+.list__title {
+  margin-bottom: 20px;
+}
+
+ul {
+  display: flex;
+  gap: 15px;
+  flex-direction: column;
+  list-style: none;
+  text-align: left;
 }
 </style>
---
+++ src/components/UI/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <div>WEB APP</div>
+    <img src="../../../src/assets/logo.jpeg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FullHeader",
+};
+</script>
+
+<style>
+.header {
+  display: flex;
+  gap: 20px;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 15px 10px 25px;
+}
+
+.header img {
+  max-width: 70px;
+  border-radius: 20px;
+}
+</style>
